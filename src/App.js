d6e4fa29fa3027afc733f805ed61b15a561906d6
Jay <rugia813@yahoo.com.tw>
2017-08-24 14:38:14 +0800

    install semantic UI

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
+    this.state = {
+      errors: [],
+      user: null
+    }
     fetch('http://quiz.net/api/register', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-      }})
+      }
+    })
       .then(response => {
-        return response.json();
+        const status = response.status;
+        if (status == 200) {
+          response.json().then((json) => {
+            this.setState({ user: json })
+          });
+        } else {
+          response.json().then((json) => {
+            this.setState({ errors: json })
+          });
+        }
       })
-      .then((json) => console.log(json))
-      .catch((error) => console.log(error));
-    
+      .catch((error) => console.log('errors: ' + error));
   }
   render() {
+    let errors = []
+    for(let i in this.state.errors){
+      errors.push(`${i}: ${this.state.errors[i]}`)
+    }
+    errors = errors.map((e, i) => { return <li value="*" role="listitem" className="" key={i}>{e}</li>})
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul role="list" className="ui list">
+          {errors}
+        </ul>
+        <Modal trigger={<Button>Show Modal</Button>}>
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content image>
+            <Image wrapped size='medium' src='/assets/images/avatar/large/rachel.png' />
+            <Modal.Description>
+              <Header>Default Profile Image</Header>
+              <p>We've found the following gravatar image associated with your e-mail address.</p>
+              <p>Is it okay to use this photo?</p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+        {this.state.user}
       </div>
     );
   }
